2a465bc91a0a795d960550e5980e6dcf2b0841a2
mateusmarcon12 <37482806+mateusmarcon12@users.noreply.github.com>
2018-04-30 13:13:53 -0300

    ajuste anuncios todos

--- agricola/app/Http/Controllers/AnuncioController.php
+++ agricola/app/Http/Controllers/AnuncioController.php
@@ -42,7 +42,7 @@ class AnuncioController extends Controller
     //exibir todos os anúncios ativos de outros usuários
     public function todosanuncios(){
         /*$anu = Anuncio::where('idanunciante','!=', Auth::user()->id)->Situacao()->orderby('created_at','desc')->join('users','users.id','=','idanunciante')->select('Anuncios.titulo', 'anuncios.datavalidade','anuncios.descricao','anuncios.tipo','users.name as username')->get();*/
-        $anu = Anuncio::join('users','users.id','=','anuncios.idanunciante')->where('idanunciante','!=', Auth::user()->id)->where('anuncios.situacao','=','ativo')->get();
+        $anu = Anuncio::join('users','users.id','=','anuncios.idanunciante')->where('idanunciante','!=', Auth::user()->id)->where('anuncios.situacao','=','ativo')->orderby('anuncios.created_at','desc')->get();
         
         return view('anuncios.todos')->with('anu', $anu);
     }
