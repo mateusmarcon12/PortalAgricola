38baafe184a032f6013a258ca18260cf7f97877d
mateusmarcon12 <37482806+mateusmarcon12@users.noreply.github.com>
2018-05-15 21:54:50 -0300

    ajuste layout excluir

--- agricola/resources/views/fotos/excluir.blade.php
+++ agricola/resources/views/fotos/excluir.blade.php
@@ -15,25 +15,26 @@
                             </div>
                         @endif
                         @isset($files)
-
+                            <div style="clear:both" class="row">
                                 @foreach($files as $f)
-                                    <div style="clear:both" class="row justify-content-center">
+                                    <div class="row float-left col-md-6">
+                                        <div style="width: 100%">
                                         <form method="POST" enctype="multipart/form-data" action="{{ route('fotos.apagar',Auth::user()->id) }}">
                                             @csrf
                                             <input type="hidden" type="text" name="images" id="file" value="{{$f}}">
                                            <img class="img-responsive rounded float-left" width="100" src="{{ url('storage/'.$f) }}"  alt="Anuncio">
 
-                                           <button type="submit" class="btn btn-danger float-right">
+                                           <button type="submit" width="100" class="btn btn-danger">
                                                         {{ __('Excluir') }}
                                                     </button></td>
 
 
                                         </form>
-
+                                        </div>
                                     </div>
                                     <br>
                                 @endforeach
-
+                            </div>
                             @endisset
                         
                     </div>
