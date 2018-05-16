93c9e7abc7f7d2d0c3427dd5ae549c6e7061e37a
mateusmarcon12 <37482806+mateusmarcon12@users.noreply.github.com>
2018-05-15 22:29:48 -0300

    imagens anuncios - excluir

--- agricola/resources/views/fotos/excluir.blade.php
+++ agricola/resources/views/fotos/excluir.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-10">
                 <div class="card">
-                    <div class="card-header">Anunciantes do Portal</div>
+                    <div class="card-header">Imagens</div>
 
                     <div class="card-body">
 
