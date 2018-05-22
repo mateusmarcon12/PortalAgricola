451b45782f0b63c5336cf8c382a03ab9dc3d0d1f
mateusmarcon12 <37482806+mateusmarcon12@users.noreply.github.com>
2018-05-22 20:42:39 -0300

    a

--- agricola/resources/views/layouts/app.blade.php
+++ agricola/resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('tablesortcss/Scripts/bootstrap-sortable.js') }}" defer></script>
+    <script src="{{ asset('tablesortcss/Scripts/moment.min.js') }}" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
