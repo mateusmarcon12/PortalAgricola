1a11f0006fe06a1f05d39087d692a5aeb22e8011
mateusmarcon12 <37482806+mateusmarcon12@users.noreply.github.com>
2018-05-23 10:28:54 -0300

    Ajustes termos em inglês

--- agricola/resources/views/auth/passwords/email.blade.php
+++ agricola/resources/views/auth/passwords/email.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header">{{ __('Trocar senha') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -18,7 +18,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Informe seu e-mail cadastrado no portal') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
@@ -34,10 +34,12 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                    {{ __('Enviar link para troca de senha') }}
                                 </button>
                             </div>
                         </div>
+                        <br>
+                        <p>O sistema enviará um e-mail para você, neste haverá um link que direcionará você para a página de redefinição de senha.</p>
                     </form>
                 </div>
             </div>
