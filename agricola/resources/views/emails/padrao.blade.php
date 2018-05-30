52430ada67a90f1fb49104c0e2aea700d28f58c1
mateusmarcon12 <37482806+mateusmarcon12@users.noreply.github.com>
2018-05-30 10:48:03 -0300

    layout e-mail

--- agricola/resources/views/emails/padrao.blade.php
+++ agricola/resources/views/emails/padrao.blade.php
@@ -3,7 +3,7 @@
 <h4>O seguinte usuário abriu negociação com você:
 {{$remetente}}</h4>
 
-<h4>Vá até o portal e continue a negociação ou finalize. enquanto a negociação estiver em aberto, o seu anuncio não será exibido para os demais anunciantes.</h4>
+<h4>Vá até o portal e continue a negociação ou finalize. enquanto a negociação em andamento, o seu anúncio não será exibido para os demais anunciantes.</h4>
 
 <h4>Mensagem:</h4>
 <p>
@@ -14,4 +14,4 @@
 Título: {{$titulo}} <br>
 Validade: {{$validade}}<br>
 @endisset
-<p>Para responder a mensagem ao usuário envie e-mail para {{$remetente}}</p>
+<p>Para responder está mensagem sem interção com o portal, envie e-mail para {{$remetente}}. Mensagens trocadas por e-mail não serão registradas na negociação dentro do portal.</p>
